29a62756cce98628ba68de2ba5b28d748eb6eb06
Danagul05 <danagul.princess.05@gmail.com>
2023-07-06 16:21:55 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
+    public static  String Walk (int age, int temperatyre) {
+        String Walk=
+        if ( age > 20 && age < 45 && temperatyre > -20 && temperatyre < 30){
+            System.out.println("Можно идти гулять");
+          else if  (age<20 && temperatyre>0 && temperatyre< 28) {
+                System.out.println("Можно идти гулять");
+             else if (age> 45 && temperatyre > -10 && temperatyre< 25) {
+                 else()
+                    System.out.println("Оставайтесь дома");
+                    return Walk;
+                }
+
+            }
+        }
+    }
